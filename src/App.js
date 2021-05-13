d44770c6ab3a0f04bbfe3d94bb5c152eda02bd69
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-05-12 23:16:22 -0500

    Met MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import "./App.css";
 
+// import Explanation from './Components/Explanation/Explanation';
+
+
 function App() {
+  
+  const [image, setImage] = useState(null)
+
+  useEffect(() =>{
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then( response => {
+      console.log(response.data)
+  
+      setImage(response.data.url)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  
+  } )
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+
+      <div className="App-header">
+      <h1>Astronomy Photo of The Day</h1>
+      </div>
+
+      <div className="img-holder">
+        <img src= {image} alt="" />
+      </div>
+
+      {/* <Explanation /> */}
+
+      <div className="button">
+        <button>click me</button>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Explanation/Explanation.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import './explanation.css';
+
+// const Explanation = props => {
+//     const { explanation } = props;
+
+//     return (
+//         <div className="explanation">
+//             <p>
+//                 {explanation.explanation}
+//             </p>
+//         </div>
+//     );
+// };
+
+
+// export default Explanation;
